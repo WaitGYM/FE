d2b4b0b5fb3c4037b7f0ad062872ada89601eca1
lyla <bgy09270@naver.com>
2025-09-29 17:31:00 +0900

    [web] 박스,텍스트 예외케이스 스타일수정

--- web/src/features/home/index.tsx
+++ web/src/features/home/index.tsx
@@ -121,7 +121,9 @@ export default function HomePage() {
                     <div className="info">
                       <p className="title">{routine.name}</p>
                       <div className="detail">
-                        {routine.isActive && <strong>운동중!!</strong>}
+                        {routine.isActive && (
+                          <div className="badge ing">운동중</div>
+                        )}
                         <span>{routine.exerciseCount}개 운동</span>
                         <span>예상시간 {routine.duration}분</span>
                       </div>
